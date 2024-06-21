ca0222727c44890ff41a9e422fb38daa13037f45
Rizwan9498 <rizwanb.tech94@gmail.com>
2024-06-21 11:34:37 +0530

    Add files via upload

---
+++ assignmrnt operator.py
@@ -0,0 +1,14 @@
+x=int(input('enter  your number1='))
+y=int(input('enter your number2='))
+z=x+y
+print('value of z is :',z)
+z+=x
+print('addition values is:',z)
+z-=x
+print('subtraction values is :',z)
+z*=x
+print('multipplication values is :',z)
+z/=x
+print('division values is:',z)
+
+
